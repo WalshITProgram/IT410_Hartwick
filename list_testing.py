8db0e42d12208a965257bba871258a9d056824a1
Joseph Hartwick <Jhartwic@mail.walshcollege.edu>
2022-05-02 10:52:47 -0400

    inital commit

---
+++ list_testing.py
@@ -0,0 +1,7 @@
+favorite_marvel_moves = ["Avengers", "Thor:Ragnorok", "Captain America: Winter Soldier", "Guardians of the Galaxy"]
+
+print(favorite_marvel_moves)
+
+favorite_marvel_moves.append("Black Panther")
+
+del favorite_marvel_moves[1]
