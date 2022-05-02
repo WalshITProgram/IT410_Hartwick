5e0ac289f2bc0d4f0146b50ea2d4d54fe2e2cd33
Joseph Hartwick <Jhartwic@mail.walshcollege.edu>
2022-05-02 12:19:44 -0400

    chapter2

---
+++ Chapter2TryItYourself.py
@@ -0,0 +1,24 @@
+# try it yourself 2-1
+# printing a message and assigning a new value to it
+restaurant = "Outback Steakhouse"
+print(restaurant)
+restaurant = "Ribeye Steak"
+print(restaurant)
+
+
+# try it yourself 2-2
+# printing a message using string concatenation
+lastBusiness = "Dairy Queen"
+lastPurchase = "Chicken basket"
+print("The last time I went to " + lastBusiness + ", I purchased " + lastPurchase)
+
+# try it yourself 2-3
+favoriteCar = "Corvette"
+
+print(favoriteCar.upper())
+print(favoriteCar.lower())
+print(favoriteCar.title())
+
+
+
+
