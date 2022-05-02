6ede2a78a336a9c71f40614c3864b46ae60c5a24
Joseph Hartwick <Jhartwic@mail.walshcollege.edu>
2022-05-02 11:14:52 -0400

    for loop

---
+++ list_TestingNew.py
@@ -0,0 +1,7 @@
+list_of_chores = ["Take out Trash", "Wash Dishes", "Cut Grass", "Dust Coffee Table"]
+
+for chore in list_of_chores[1:3]:
+    chore = chore + " (10 minutes)"
+    print(chore + " - Done!")
+
+
