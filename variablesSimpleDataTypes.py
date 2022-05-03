e56a760b32469a6f347fb1266739692821270651
Joseph Hartwick <Jhartwic@mail.walshcollege.edu>
2022-05-03 09:18:00 -0400

    simple data types

---
+++ variablesSimpleDataTypes.py
@@ -0,0 +1,38 @@
+# declare first name step 1
+first_name = "Joseph".lower()
+
+# declare last name step 2
+last_Name = "Hartwick".upper()
+
+# print first name and last name step 3
+print("Hello, " + first_name.upper() + " " + last_Name.lower())
+
+# print 2 newlines step 4
+print("\n\n")
+
+# print a quote step 5
+print("'Start by doing whats necessary; then do what's possible; and suddenly you are \n doing the impossible - Francis of Assisi'")
+
+# declare decimal numbers step 6
+number1 = 4.5
+number2 = 6.5
+
+# declare addition, subtraction, multiplication and divsion variables step 7
+addition_value = number1 + number2
+subtraction_value = number1 - number2
+multiplication_value = number1 * number2
+division_value = number1 / number2
+
+# print out results step 8
+print(number1, "plus",  number2, "equals", addition_value)
+print(number1, "minus",  number2, "equals", subtraction_value)
+print(number1, "times",  number2, "equals", multiplication_value)
+print(number1, "divided by",  number2, "equals", division_value)
+
+# declare current month and number of day step 9
+current_month = "May"
+current_day = 3
+
+# output current month and day step 10
+print("\t\tToday is day", current_day, "of the month of", current_month)
+
