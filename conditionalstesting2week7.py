db51f4bdadc45861f4826b4114895487076d578b
Joseph Hartwick <Jhartwic@mail.walshcollege.edu>
2022-05-09 11:53:48 -0400

    comparing lists

---
+++ conditionalstesting2week7.py
@@ -0,0 +1,10 @@
+list1 = ["one", "two", "three"]
+list2 = ["one", "apple", "three", "four"]
+
+#list1 = [1, 2, 3]
+#list2 = ["1", "2", "3"]
+
+
+
+if len(list1) < len(list2):
+    print("Lists 2 is bigger in terms of number of values than list 1")
