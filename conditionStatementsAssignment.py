2395ee89087d3cfaba946c3a8c7d839babcad149
Joseph Hartwick <Jhartwic@mail.walshcollege.edu>
2022-05-13 13:05:10 -0400

    Completed Condition Statements Assignment

--- conditionStatementsAssignment.py
+++ conditionStatementsAssignment.py
@@ -1,3 +1,4 @@
+# declare employee data list step #1
 employeeData = [1121, "Jackie Grainger", 22.22,
  1122, "Jignesh Thrakkar", 25.25,
  1127, "Dion Green", 28.75, False,
@@ -9,12 +10,13 @@ employeeData = [1121, "Jackie Grainger", 22.22,
  "Jackie Grainger", 1121, 22.22, False,
  22.65, 1152, "David Toma"]
 
-# declare empty lists that we will add data into later
+# declare empty lists that I will add data into later step #2A
 employeeNumbers = []
 employeeNames = []
 employeeSalary = []
 
-# for loop to sort the data into their respective lists
+# for loop to sort the data into their respective lists 
+# step #2b and step 3
 for data in employeeData:
     if data not in employeeNumbers and type(data) == int:
         employeeNumbers.append(data)
@@ -22,34 +24,37 @@ for data in employeeData:
         employeeNames.append(data)
     elif data not in employeeSalary and type(data) == float:
         employeeSalary.append(data)
-# declare total hourly rate list
+# declare total hourly rate list step 4A
 total_hourly_rate = []
-# loop to multiply employee salary by 1.3 
+
+# loop to multiply the employee's salary by 1.3 step 4b 
 for count in range(0,len(employeeSalary)):
     newSalary = employeeSalary[count] * 1.3
     total_hourly_rate.append(newSalary)
 maxValue = 0
 
-# for loop to determine the max total hourly rate value
+# for loop to determine the max total hourly rate value step 4c
 for count in range(0,len(total_hourly_rate)):
     if total_hourly_rate[count] > maxValue:
         maxValue = total_hourly_rate[count]
 
-# edit this later joe! and delete this comment 
+# determine max value step 4d
 if maxValue > 37.30:
-    print(maxValue, "'s employee salary could be a budget concern")
+    print(f"${maxValue} could be a budget concern regarding salary")
 
-# declare underpaid salaries list
+# declare underpaid salaries list step 5a
 underpaid_salaries = []
-# for loop to fill the underpaid salaries list
-for count in range(0,len(total_hourly_rate)):
-    if (total_hourly_rate[count] > 28.15) and (total_hourly_rate[count] < 30.65):
-        underpaid_salaries.append(total_hourly_rate[count])
-# declare company raises list
-companyRaises = []
-print(employeeSalary)
 
-# for loop to fill the company raises 
+# for loop to fill the underpaid salaries list step 5b 
+for count in range(0,len(total_hourly_rate)):
+    if total_hourly_rate[count] > 28.15 and total_hourly_rate[count] < 30.65:
+        underpaid_salaries.append(total_hourly_rate[count])
+
+# declare company raises list step 6a
+companyRaises = []
+
+
+# for loop to fill the company raises list step 6b
 for count in range(0,len(employeeSalary)):
     if (employeeSalary[count] >= 22) and (employeeSalary[count] <= 24):
         newSalary = employeeSalary[count] * 1.05
@@ -63,7 +68,17 @@ for count in range(0,len(employeeSalary)):
     else:
         newSalary = employeeSalary[count] * 1.02
         companyRaises.append(newSalary)
-print(companyRaises)
+
+
+
+# finding the person with the highest salary in the list and findind his id number and information in the list.  
+# Then since he has the highest salary, he is supervisor step 7
+for count in range(0,len(employeeSalary)):
+    if employeeSalary[count] >= 26 and employeeSalary[count] <= 29 and employeeNames[count] == "Dion Green" and employeeNumbers[count] == 1127:
+        print(f"Our supervisors name is: {employeeNames[count]}.\nHis salary is ${employeeSalary[count]}.\nFinally, his employee id number is: {employeeNumbers[count]}")
+
+
+
 
 
 
