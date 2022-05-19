bc42de4018768b3bbafefa11b78f152574f0fc66
Joseph Hartwick <Jhartwic@mail.walshcollege.edu>
2022-05-19 09:48:06 -0400

    added problem 6-1 and 6-2

---
+++ chapter6problemsweek8.py
@@ -0,0 +1,11 @@
+#problem 6-1
+#declare dictionary for the new doctor strange movie 
+doctorStrangeMovie = {"Movie_Title": "Doctor Strange in the Multiverse of Madness", "movie_rating": "PG-13", "director": "Sam Raimi",  "year_released" : 2022, "writer" : "Michael Waldron", "user_rating" : "7.4/10"}
+
+# problem 6-2
+hpEnvy = {"Item Number": 4156, 
+        "Product Name" : "HP Envy", 
+        "Price" : 1050}
+hpEnvy["Price"] *= 1.30
+
+print(hpEnvy["Price"])
